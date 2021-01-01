f7f04d738e645a29b1b0e93d7604a30d1a9c3a3d
Irenej Jerič <irenej.jeric@outlook.com>
2021-01-01 22:32:21 +0100

    Additional styling implemented (grids, colors). Nested selectors still not implemented (recursive funcitons needed).

--- src/selectors.jsx
+++ src/selectors.jsx
@@ -1,12 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BsArrowReturnRight, BsPlusCircle, BsXSquare } from "react-icons/bs";
 
 const Selector = props =>  {
 const {onDelete, onAdd, onAddChild, onInputChange, index, inputValue, children} = props;
   
   return (
-    <Fragment>
+    <div className="selector-list wrapper">
+      <div className="selector-list-label">
         <label>Selector {index +1 }: </label>
+      </div>
+      <div className="selector-list-input">
         <input 
           type="text"
           value={inputValue}
@@ -14,20 +17,23 @@ const {onDelete, onAdd, onAddChild, onInputChange, index, inputValue, children} 
           onChange={(input) => onInputChange(input, "selector")}
           id={index + 1}>
           </input>
+      </div>
+      <div className="selector-list-control-buttons">
+        <BsArrowReturnRight
+          className="addChildButton button" 
+          onClick={onAddChild}/>
         <BsPlusCircle
           className="addSelectorButton button" 
           data-title="Add a new selector for this list."
           onClick={onAdd} />
-        <BsArrowReturnRight
-          className="addChildButton button" 
-          onClick={onAddChild}/>
         {/*Add Delete button only if the number of line items is greater than one.*/}
         {(children > 1) ? (<BsXSquare
           className="deleteSelectorButton button" 
           title="Delete the selector"
           onClick = {onDelete}/>)
           :null}
-    </Fragment>
+      </div>
+    </div>
     )
   }
   
--- src/urlInput.jsx
+++ src/urlInput.jsx
@@ -1,32 +1,38 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BiTrash } from "react-icons/bi";
 
 const UrlInput = props => {
   const { urlValue, nameValue, onInputChange, onSiteDelete, urlProp, nameProp, siteMaps } = props
   return (
-    <Fragment>
-    <div className="listHeader">
-      <label>List name: </label>
+    <div className="list wrapper">
+      <div className="list-name-label">
+        <label>List name: </label>
+      </div>
+      <div className="list-name-input">
+          <input 
+            type="text"
+            placeholder= "Enter a name"
+            value = {nameValue}
+            onChange= {(input) => onInputChange(input, nameProp)}/>
+      </div>
+      <div className ="list-URL-label">
+        <label>URL: </label>
+      </div>
+      <div className="list-URL-input">
         <input 
           type="text"
-          placeholder= "Enter a name"
-          value = {nameValue}
-          onChange= {(input) => onInputChange(input, nameProp)}
-        />
-      <label>URL: </label>
-      <input 
-        type="text"
-        placeholder="Enter URL address"
-        value = {urlValue}
-        onChange = {(input) => onInputChange(input, urlProp)}
-      />
+          placeholder="Enter URL address"
+          value = {urlValue}
+          onChange = {(input) => onInputChange(input, urlProp)}/>
+      </div>
       {/*Add Delete button only if the number of siteMaps is greater than one.*/}
-      {(siteMaps > 1) ? (<BiTrash 
+      <div className="list-delete-button">
+       {(siteMaps > 1) ? (<BiTrash 
           className="deleteList button" 
           onClick= {() => onSiteDelete()}/>)
-        : null}
+          : null}
+      </div>
     </div>
-    </Fragment>
    );
 }
  

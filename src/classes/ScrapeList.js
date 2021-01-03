43b118823f7e45ebaa6fac866271c38e2ff3a835
Irenej Jerič <irenej.jeric@outlook.com>
2021-01-03 21:25:01 +0100

    First outline of new nestign logic implemented. Adding of childselectors is possible, hierarchy can be read through properties of childOf nad parentOf. Code needs cleanup (too imperative).

--- src/classes/ScrapeList.js
+++ src/classes/ScrapeList.js
@@ -1,14 +1,27 @@
 import { nanoid } from "nanoid";
 
 export class Selector {
-  constructor(id = nanoid()) {
+  constructor(id=nanoid()) {
     this.id = id;
     this.value= "";
     this.multiple= "";
     this.componentClass = "selector"
     this.type = "";
+    this.childOf = [];
+    this.parentOf = [];
     this.scrapeResult=[];
-    this.selectors = [];
+  }
+  addParentStatus(childID) {
+    this.parentOf.push(childID);
+  }
+  removeParentStatus(parentID) {
+    this.parentOf = this.parentOf.filter(item => item !== parentID);
+  }
+  addChildStatus(parentID) {
+    this.childOf.push(parentID);
+  }
+  removeChildStatus(childID) {
+    this.childOf = this.childOf.filter(item => item !== childID);
   }
 }
 
@@ -18,51 +31,50 @@ class SiteMap {
     this.url= "";
     this.componentClass="sitemap";
     this.name = name;
-    this.selectors= [new Selector()];
-  }
+    this.parentOf = [nanoid()]
+    this.selectors= [this.addFirstSelector(this.parentOf[0], this.id)];
+    }
+    
+    addFirstSelector (childID, parentID) {
+      let selector = new Selector(childID);
+      selector.childOf = parentID;
+      return selector;
+    }
 
   
-  
-  //Return nested selector by id
-  checkChildren(arr, id, Action) {
-    let isArray = Array.isArray(arr);
-    if (isArray) {
-      arr.map(item => {
-        if (item.id === id) {
-           return Action(item);
-          } else {
-            if (item.selectors.length) { this.checkChildren(item.selectors, id, Action)};
-          }
-        });
-      } else {
-        if (arr.id === id) {
-            return Action(arr);
-        } else {
-          if (arr.selectors.length) { this.checkChildren(arr.selectors, id, Action) };
-      }
-    }
-  }
-    
   //Add a selector to the current SiteMap. 
   //HOW IT WORKS: You ADD A SIBLING selector by referencing the id of the PARENT (i.e. a sitemap or a parent selector id). You ADD A CHILD by referencing the id of the SELECTOR ITSELF.
-  addSelector(id, index=0) {
+  addSelector(parentID, index=0) {
     let state = this;
+    let childID = nanoid();
 
-    function SelectorAdd(result) {
-      if (index >= result.selectors.length - 1 || result.selectors.length === 0) {
-          return result.selectors.push(new Selector());
+    if (index >= state.selectors.length - 1 || state.selectors.length === 0) {
+          state.selectors.push(new Selector(childID));
       } else {
         let newSelectors = [
-          ...result.selectors.slice(0, index + 1),
-          new Selector(),
-          ...result.selectors.slice(index + 1)
+          ...state.selectors.slice(0, index + 1),
+          new Selector(childID),
+          ...state.selectors.slice(index + 1)
         ];
-        return result.selectors = newSelectors;
-        }
+        state.selectors = newSelectors;
       }
-      this.checkChildren(state, id, SelectorAdd);
-      this.selectors = state.selectors;
+      
+    //are added selectors children of siteMap or children of selectors? Act accordingly.
+    let selectors = this.selectors;
+    let childIndex, parentIndex;
+    if (this.id === parentID) {
+        this.parentOf.push(childID);
+        childIndex = selectors.findIndex(item => item.id === childID);
+        selectors[childIndex].addChildStatus(parentID);
+    } else {
+        childIndex = selectors.findIndex(item => item.id === childID);
+        parentIndex = selectors.findIndex(item => item.id === parentID);
+        console.log("Parent", selectors[parentIndex]);
+        selectors[childIndex].addChildStatus(parentID);
+        selectors[parentIndex].addParentStatus(childID);
     }
+  }
+
   
 
   deleteSelector(id) {
@@ -95,6 +107,28 @@ class SiteMap {
     updatedSiteMap[property] = input;
     this.SiteMap = updatedSiteMap;
   }
+
+  //Return nested selector by id. Commented out because we will be parsing
+  //the nested selector via the childOf property instead of actual nesting of arrays.
+  /*checkChildren(arr, id, Action) {
+    let isArray = Array.isArray(arr);
+    if (isArray) {
+      arr.map(item => {
+        if (item.id === id) {
+           return Action(item);
+          } else {
+            if (item.selectors.length) { this.checkChildren(item.selectors, id, Action)};
+          }
+        });
+      } else {
+        if (arr.id === id) {
+            return Action(arr);
+        } else {
+          if (arr.selectors.length) { this.checkChildren(arr.selectors, id, Action) };
+      }
+    }
+  }
+  */
 
   //Add a child selector at a specified index. Disable, becuas ethe addSelector
   //function already achieves that.

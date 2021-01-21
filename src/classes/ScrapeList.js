16a26eb25081f6b7017271a252f5f253913ce8be
Irenej Jerič <irenej.jeric@outlook.com>
2021-01-21 20:32:03 +0100

    Prettier reformatting of ScrapeList.js. After merging, the ScrapeList logic needs to be optimized.

--- src/classes/ScrapeList.js
+++ src/classes/ScrapeList.js
@@ -37,11 +37,11 @@ class Selector {
 //------------------------
 //------------------------
 
-function AddSelector(state, parentID, index,childID = nanoid()) {
-  if (state.find((item) => item.id === childID)) {
+function AddSelector(state, parentID, index, childID = nanoid()) {
+  if (state.find(item => item.id === childID)) {
     return console.log("Error: a child with this id already exists!");
   }
-  let parent = state.filter((x) => x.id === parentID)[0];
+  let parent = state.filter(x => x.id === parentID)[0];
   if (!parent) {
     return console.log("Error: Parent does not exist!");
   }
@@ -68,19 +68,19 @@ function AddSelector(state, parentID, index,childID = nanoid()) {
   //until an item with the componentClass === "sitemap" is found. This method presumers
   //that only one parent of a child can exist, even though the array type of the cildOf
   //property indicates the possibility of future implementation of shared parents.
-  const findSiteMap = (parent) => {
+  const findSiteMap = parent => {
     parent.componentClass === "sitemap"
       ? (newSelector.memberOfSiteMap = parent.id)
-      : findSiteMap(state.filter((item) => item.id === parent.childOf[0])[0]);
+      : findSiteMap(state.filter(item => item.id === parent.childOf[0])[0]);
   };
   findSiteMap(parent);
 
   //Now, push the new selector to the state.
   state.push(newSelector);
   //If new selector is added, also update the parent.
-  state.map((item) => {
+  state.map(item => {
     if (item.id === parentID) {
-       return item = parent;
+      return (item = parent);
     }
   });
 }
@@ -92,28 +92,28 @@ function DeleteItem(state, id) {
   //------------------------
   //Recursively remove children of a deleted selector.
   const RecursiveDelete = (arr, id) => {
-    const target = arr.filter((item) => item.id === id);
+    const target = arr.filter(item => item.id === id);
 
     //Filter out the selector
-    newState = arr.filter((item) => item.id !== id);
+    newState = arr.filter(item => item.id !== id);
 
     //Find parent and remove reference on first iteration (in subsequent
     //iterations the parent is already removed at this point).
     if (count === 1) {
-      let parent = arr.filter((item) => item.parentOf.includes(id));
+      let parent = arr.filter(item => item.parentOf.includes(id));
       if (parent.length) {
         parent = parent[0];
-        let parentIndex = arr.findIndex((item) => item.parentOf.includes(id));
-        parent = parent.parentOf.filter((item) => item !== id);
+        let parentIndex = arr.findIndex(item => item.parentOf.includes(id));
+        parent = parent.parentOf.filter(item => item !== id);
         newState[parentIndex].parentOf = parent;
       }
     }
     count++;
 
     //Extract child id's and run the Remove script for each of them.
-    const children = target.map((item) => item.parentOf).flat();
+    const children = target.map(item => item.parentOf).flat();
     if (children.length) {
-      children.forEach((element) => RecursiveDelete(newState, element));
+      children.forEach(element => RecursiveDelete(newState, element));
     }
     return newState;
   };
@@ -124,8 +124,8 @@ function DeleteItem(state, id) {
 function ClearSiteMap(arr, siteMapID) {
   //Filter out all the selectors, whose memberOfSiteMap property equals siteMapID.
   //Additionally, remove child entries in the siteMap node. This  function needs to be // invoked as an assignment operation (arr = ClearSiteMap(array, siteMapID)) .
-  let newArray = arr.filter((item) => item.memberOfSiteMap !== siteMapID);
-  newArray[newArray.findIndex((item) => item.id === siteMapID)].parentOf = [];
+  let newArray = arr.filter(item => item.memberOfSiteMap !== siteMapID);
+  newArray[newArray.findIndex(item => item.id === siteMapID)].parentOf = [];
   return newArray;
 }
 
